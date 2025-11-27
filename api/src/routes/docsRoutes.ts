a22c5c6fb27eefb845a8e281d3711a52e9b8c544
Mateus ieno <125301838+MateusIR@users.noreply.github.com>
2025-11-27 17:07:40 -0300

    teste2

--- api/src/routes/docsRoutes.ts
+++ api/src/routes/docsRoutes.ts
@@ -1,15 +1,22 @@
 import { Router } from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../../swagger.json' with { type: "json" };
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const swaggerDocument = require('../../swagger.json');
 
 const router = Router();
 
 const options = {
-  customCss: '.swagger-ui .topbar { display: none }',
+  customCssUrl: 'https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css',
+  customJs: [
+    'https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js',
+    'https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-standalone-preset.js'
+  ],
   customSiteTitle: "Estocando API - Documentação",
+  customCss: '.swagger-ui .topbar { display: none }' 
 };
 
-// Configuração padrão do Swagger UI
 router.use('/', swaggerUi.serve);
 router.get('/', swaggerUi.setup(swaggerDocument, options));
 
