75cb289eae2807802fcd2b08452184eaee299f63
Mateus ieno <125301838+MateusIR@users.noreply.github.com>
2025-11-27 16:33:24 -0300

    .

--- api/src/routes/docsRoutes.ts
+++ api/src/routes/docsRoutes.ts
@@ -6,26 +6,15 @@ import path from 'path';
 
 const router = Router();
 
-const swaggerFilePath = path.resolve(process.cwd(), 'swagger.yaml');
-const fileContent = fs.readFileSync(swaggerFilePath, 'utf8');
-const swaggerDocument = YAML.parse(fileContent);
 
-//-------------------------------------------------------------------------
-if (process.env.CODESPACE_NAME) {
-  const codespaceName = process.env.CODESPACE_NAME;
-  const port = process.env.PORT || 3333;
-  // Constrói a URL pública que o Codespaces cria
-  const serverUrl = `https://${codespaceName}-${port}.app.github.dev`;
-  
-  console.log(`✅ Detectado ambiente Codespaces. Configurando URL do servidor para: ${serverUrl}`);
-  
-  // Substitui a URL no documento do Swagger dinamicamente
-  if (swaggerDocument.servers && swaggerDocument.servers.length > 0) {
-    swaggerDocument.servers[0].url = serverUrl;
-    swaggerDocument.servers[0].description = "Servidor do GitHub Codespaces";
-  }
-}
-// ------------------------------------------------------------------
+const filePath = path.join(process.cwd(), "swagger.yaml");
+
+
+const swaggerFile = fs.readFileSync(filePath, "utf8");
+
+
+const swaggerDocument = YAML.parse(swaggerFile);
+
 const options = {
   customCss: '.swagger-ui .topbar { display: none }',
   customSiteTitle: "Estocando API - Documentação",
