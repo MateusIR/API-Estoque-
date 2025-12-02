10daf970e88b82033d6f9343369de5d7a29da7e3
Mateus ieno <125301838+MateusIR@users.noreply.github.com>
2025-12-02 16:17:33 -0300

    a

--- front/src/pages/AdjustStock.tsx
+++ front/src/pages/AdjustStock.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { itemService } from '../services/item';
 import { adjustStockSchema } from '../schemas';
